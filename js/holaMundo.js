323c6964e990b74251a204e594ce8f3136f8f547
Andy <andysatie@gmail.com>
2021-08-12 14:15:50 -0300

    Ejercicios hasta el numero 5

--- js/holaMundo.js
+++ js/holaMundo.js
@@ -1,3 +1,4 @@
+
 //Esto es un comentario simple o de una sola linea.
 
 /* Esto es un comentario
@@ -5,24 +6,59 @@ de multiples
 lineas */
 
 //mostrar un mensaje en consola.
-console.log("Hola comisión 16i");
+    // console.log("Hola comisión 16i");
 
 //mostrar un msj en el html
-document.write("Este es mi primer mensaje.")
+    // document.write("Este es mi primer mensaje.")
 
 //mostrar un msj en una ventana emergente.
 //alert("Hola mundo")
 
 //crear una variable (var - let - const)
 
-let comision = "16i";
+    // let comision = "16i";
 
-document.write("<br>Comision: "+ comision)
+    // document.write("<br>Comision: "+ comision)
 
 //modificar el contenido de una variable.
-comision = "i";
+    // comision = "i";
 
-document.write("<br>Comision: "+ comision)
+    // document.write("<br>Comision: "+ comision)
 
-const year = 2021;
-document.write("<br>Año: "+ year)
+    // const year = 2021;
+    // document.write("<br>Año: "+ year)
+
+/* Ejercicios.
+1.- Escribe un programa de una sola línea
+que haga que aparezca en la pantalla un alert
+que diga “un mensaje”. */
+
+alert("un mensaje")
+
+/*2.- Escribe un programa de una sola línea
+que escriba en la pantalla un texto
+que diga «Hello World» (document.write). */
+
+document.write("«Hello World» <b>(document.write)</b> </br>")
+
+/*3.- Escribe un programa de una sola línea
+que escriba en la pantalla el resultado de sumar 3 + 5.*/
+
+document.write(3+5)
+
+/*4.- Escribe un programa de dos líneas
+que pida el nombre del usuario con un prompt
+y escriba un texto que diga «Hola nombreUsuario»*/
+
+let nombre = prompt("¿Cuál es tu nombre?");
+document.write("<br>¡Hola "+nombre+"!<br>")
+
+/*5.- Escribe un programa de tres líneas
+que pida un número, pida otro número
+y escriba el resultado de sumar estos dos números.*/
+
+let numeroUno = prompt("Escribe un número.")
+let numeroDos = prompt("Escribe otro.")
+document.write(parseInt(numeroUno)+parseInt(numeroDos))
+
+
