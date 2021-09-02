a27d69da253c88d6e3d5dc1a94b2dd0515c40ab9
Andy <andysatie@gmail.com>
2021-09-02 02:17:58 -0300

    se agregó objetos.js

---
+++ js/objetos.js
@@ -0,0 +1,13 @@
+let nombre = "tonty strike";
+let numeroSerie = "123gsd2";
+let precio = 5000;
+let version = "End-game";
+
+//crear un objeto con NOTACION LITERAL
+let funkoIronMan = {
+  // clave: valor => son PROPIEDADES
+  nombre: "tonty strike",
+  numeroSerie: "123gsd2",
+  precio: 5000,
+  version: ["End-game", "IronMan1"],
+};
