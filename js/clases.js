87f2d6c7b760c833c0370de54b91b7332b3fbdc3
Andy <andysatie@gmail.com>
2021-09-02 11:38:50 -0300

    se agregó el archivo de clases

---
+++ js/clases.js
@@ -0,0 +1,7 @@
+//crear una clase
+class Funkopop{
+    //acá comienzo a crear mi clase o 'molde'
+    constructor(nombreFunko, precio, estado, version, numSerie){
+        this.nombre = nombreFunko;
+    }
+}
