c6e287be5e83ffe01280940a985d5ef764ae0f65
Andy <andysatie@gmail.com>
2021-09-17 14:25:51 -0300

    se comenzó a realizar el ej 7 de POO

--- js/tp4POOclass/ejercicio7.js
+++ js/tp4POOclass/ejercicio7.js
@@ -27,56 +27,148 @@ Crea un menú con opciones por consola para probar todas estas funcionalidades.
 */
 
 //let agenda = new Agenda(10)
-let listaContacto = []
 
-class Contacto{
-    constructor(nombre, telefono){
-        this.nombre = nombre;
-        this.telefono = telefono;
-    }
-    aniadirContacto(contacto){
-        if(listaContacto.length < 10){
-            listaContacto.push(contacto)
-        }
-
-    }
-    existeContacto(contacto){ //usar .find
-        if(listaContacto.includes(contacto)){
-            document.write(`${contacto} ya existe.`);
-        }else{
-            return document.write(`${contacto} no existe.`)
-        }
-    }
-    listarContactos(){ //crear bucle simple
-        let claves = Object.keys(listaContacto); //['nombres', 'numserie', ...] (en forma de arreglo)
-        console.log(claves.length);
-        document.write("<br>FunkoIronMan<br>");
-    for (let posicion = 0; posicion < claves.length; posicion++) {
-    //obtengo el nombre de una de las claves por cada vuelta del for
-    let clave = claves[posicion];
-    document.write(`<br>${clave}: ${listaContacto[clave]}`);
+class Contacto {
+  constructor(nombre, telefono) {
+    this.nombre = nombre;
+    this.telefono = telefono;
   }
 
-    }
-    buscarContacto(nombre){ //pedir nombre, guardar en variable y usar bucle para recorrer y comparar
-        //similar a existeContacto (find)
-    }
-    eliminarContacto(contacto){ //primero buscarlo y después eliminarlo con .splice O FILTER (EMILSE USÓ FILTER)
+  mostrarDatos() {
+    document.write(`Nombre: ${this.nombre} Telefono ${this.telefono}`);
+  }
 
-    }
-    agendaLlena(){ //el tamaño se guardó anteriormente en una variable, hay que compararlo con un length
-        //devuelve true o false
-    }
-    huecosLibres(){
-        //si se cumple la condición de que la cantidad de elementos de la agenda es < se hace (elementos - contactos.length)
-    }
-    opciones(){
+  get obtenerNombre() {
+    return this.nombre;
+  }
+  get obtenerTelefono() {
+    return this.telefono;
+  }
 
-    }
+  set modificarNombre(nuevoNombre) {
+    this.nombre = nuevoNombre;
+  }
+  set modificarTelefono(nuevoTelefono) {
+    this.telefono = nuevoTelefono;
+  }
 }
 
-let andy = new Agenda('Andy', 1140577540);
-listaContacto.push(andy);
-andy.listarContactos();
+class Agenda {
+  constructor(tamanio) {
+    this.contactos = [];
+    this.tamanio = tamanio;
+  }
 
-//menú con do while (en el do va el switch, en el while un confirm)
+  set modificarTamanio(cantidad) {
+    this.tamanio = cantidad;
+  }
+
+  agregarContacto(contacto) {
+    if (this.existeContacto(contacto)) {
+      console.log("El contacto ya existe, no se puede agregar.");
+    } else {
+      if (!this.agendaLlena()) {
+        this.contactos.push(contacto);
+        console.log("Contacto añadido");
+        this.listarContactos();
+      }
+    }
+  }
+
+  existeContacto(contacto) {
+    const exist = this.contactos.find(
+      (contactoExistente) => contactoExistente.nombre === contacto.nombre
+    );
+    if (exist) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  listarContactos() {
+    for (let i = 0; i < this.contactos.length; i++) {
+      console.log(
+        `Contacto: ${this.contactos[i].obtenerNombre}
+                    Telefono: ${this.contactos[i].obtenerTelefono}`
+      );
+    }
+  }
+
+  buscarContacto(contacto) {
+    const encontrado = this.contacto.find(
+      (existentes) => existentes.nombre === this.contactos
+    );
+    if (encontrado) {
+      console.log("Contacto encontrado.");
+    } else {
+      console.log("Este contacto no existe.");
+    }
+  }
+
+  eliminarContacto(contacto) {
+    let contactosFiltrados = this.contactos.filter(
+      (contactoExistente) => contactoExistente.nombre != contacto.nombre
+    );
+    console.log("Los contactos que deberían quedar" + contactosFiltrados);
+  }
+
+  agendaLlena() {
+    if (thiss.contactos.length === this.tamanio) {
+      console.log("Agenda llena");
+      return true;
+    } else {
+      console.log("Queda espacio disponible");
+      return false;
+    }
+  }
+
+  huecosLibres() {
+    let cantidadEspacios = this.tamanio - this.contactos.length;
+    console.log("Cantidad de espacio disponible" + cantidadEspacios);
+  }
+}
+let agendaPrueba = new Agenda(10);
+
+do {
+  let opcion = parseInt(
+    prompt(`Seleccione una opcion:
+            <br>1.- Agregar contacto.
+            <br>2.- Cambiar tamaño de agenda.
+            <br>3.- Listar contactos.
+            <br>4.- Buscar contacto.`)
+  );
+
+  switch (opcion) {
+    case 1:
+      agendaPrueba.huecosLibres();
+
+      let nombre = prompt("Ingrese un nombre");
+      let telefono = prompt("Ingrese un telefono");
+      let nuevoContacto = new Contacto(nombre, telefono);
+
+      agendaPrueba.agregarContacto(nuevoContacto);
+      break;
+
+    case 2:
+      let nuevoTamanio = parseInt(
+        prompt("Ingrese la cantidad de contactos que desea tener.")
+      );
+      agendaPrueba.modificarTamanio = nuevoTamanio;
+      console.log(
+        "Tamaño de agenda modificado. Cantidad actual: " + nuevoTamanio
+      );
+      break;
+
+    case 3:
+      agendaPrueba.listarContactos();
+      break;
+
+    case 4:
+      let contactoBuscado = prompt("INgrese el nombre que desea buscar");
+      agendaPrueba.buscarContacto(contactoBuscado);
+      break;
+    default:
+      document.write("Ingresó una opción invalida");
+  }
+} while (confirm("¿Quiere realizar otra operación?"));
